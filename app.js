4a75a80339e44c10237c811f78de3fb1f9b7e808
Yuguang Wang <yuwang@redhat.com>
2013-01-16 14:43:36 +0800

    use express framework to serve static.

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
 var app = require('express')()
-  , server = require('http').createServer(app)
-  , io = require('socket.io').listen(server);
+  , server = require('http').createServer(app);
 
-server.listen(8080);
-console.log("Express server listening on http://localhost:8080/");
+var express = require('express')
+  , http = require('http')
+  , path = require('path');
 
-app.get('/', function (req, res) {
+app.configure(function(){
+  app.set('port', process.env.PORT || 8000);
+  // app.set('views', __dirname + '/views');
+  // app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser('your secret here'));
+  app.use(express.session());
+  app.use(express.static(path.join(__dirname, '.')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', function (req, res){
   res.sendfile(__dirname + '/template.html');
 });
 
+var server = http.createServer(app);
+server.listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+
+var io = require('socket.io').listen(server);
 io.sockets.on('connection', function (socket) {
   socket.emit('newclient', { msg : 'welcome' });
   socket.on('news', function (data) {
